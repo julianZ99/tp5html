3c354080ffe6d121b87621ada8097edfd175f595
julianZ99 <128180017+julianZ99@users.noreply.github.com>
2023-10-04 22:04:59 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+
+
+const regExp=new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+");
+const regExpId=new RegExp("[0-9]");
+
 async function combinarEmployeesConCompanies(employees, companies) {
     return employees.map(employee => {
         const company = companies.find(company => company.companyId === employee.companyId);
@@ -34,7 +39,7 @@ async function loadTableEmployee() {
         const [employees, companies] = await Promise.all([obtenerEmployees(), obtenerCompanies()]);
         const employeesConCompanies = await combinarEmployeesConCompanies(employees, companies);
         console.log(employeesConCompanies);
-        
+
         let tableBody = document.createElement('tbody'); // Crear el tbody
         employeesConCompanies.forEach(employee => {
             const row = document.createElement('tr');
@@ -60,12 +65,12 @@ async function showEmployeesAsync() {
         oldTable.remove();
     }
 
-    let daddy=document.getElementById('tableDaddy');
+    let daddy = document.getElementById('tableDaddy');
 
-    let newTable=document.createElement('table');
-    newTable.className='table table-striped';
-    newTable.id='dataTable';
-    newTable.innerHTML=`
+    let newTable = document.createElement('table');
+    newTable.className = 'table table-striped';
+    newTable.id = 'dataTable';
+    newTable.innerHTML = `
     <thead>
       <tr>
           <th scope="col">N</th>
@@ -85,4 +90,91 @@ async function showEmployeesAsync() {
     newTable.appendChild(employeeData);
 
     daddy.appendChild(newTable);
+}
+
+//esta funcion se encarga de crear un  Json de un empleado y hacer el post de los datos a la API
+function postEmployee() {
+    let employeeData = [
+        document.getElementById('firstName').value,
+        document.getElementById('lastName').value,
+        document.getElementById('mail').value,
+        document.getElementById('selectedCompanyId').value
+    ]
+
+    let p = document.getElementById('addedEmployee');
+    p.innerText = '';
+
+    if (regExp.test(employeeData[2]) == true) {
+        let newEmployee = JSON.stringify({
+            "companyId": Number.parseInt(employeeData[3]),
+            "firstName": employeeData[0],
+            "lastName": employeeData[1],
+            "email": employeeData[2],
+        });
+
+        apiInteraction('POST', "https://utn-lubnan-api-1.herokuapp.com/api/Employee", newEmployee)
+            .then((response) => {
+                p.innerText = 'Employee ' + employeeData[0] + ' ' + employeeData[1] + ', added succesfuly';
+                p.style.color = "green";
+            })
+            .catch((reason) => {
+                console.log(Error(reason));
+                p.innerText = 'Cannot add employee, please try later';
+                p.style.color = "red";
+            })
+
+    } else {
+        p.innerText = 'invalid email';
+        p.style.color = "red";
+    }
+
+}
+
+function deleteEmployee() {
+    let id = document.getElementById("firedEmployeeId").value;
+    let p = document.getElementById("afterFireComunication");
+    p.innerText = '';
+    if (regExpId.test(id) == true) {
+        let apiLink = "https://utn-lubnan-api-1.herokuapp.com/api/Employee/" + id;
+        apiInteraction('DELETE', apiLink, null)
+            .then((response) => {
+                console.log(response);
+                p.innerText = 'Succesfully fired';
+                p.style.color = "green";
+            })
+            .catch((reason) => {
+                console.log(Error(reason));
+                p.innerText = 'the sindicate has show up, you cant fire this employee, at least for now...';
+                p.style.color = "red";
+            })
+    } else {
+        p.innerText = 'please only put numbers in the box';
+        p.style.color = "red";
+    }
+
+
+}
+
+function apiInteraction(HTTPMethod,url,body){
+    return new Promise((resolve,reject)=>{
+      var request=new XMLHttpRequest();
+      request.open(HTTPMethod,url);
+      request.onload=()=>{
+        if(request.readyState == 4 && request.status == 200){
+          if(request.responseText!=''){
+            resolve(JSON.parse(request.responseText))
+          }else{
+            resolve(request.responseText);
+          }
+        }else{
+          reject(`Error: ${request.status}`);
+        }
+      }
+      if(HTTPMethod=='POST'){
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        request.send(body);
+      }else{
+        request.send();
+      }
+    })
 }
